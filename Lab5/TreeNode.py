f808f20a35745a19755af8fdc32fe5b1b77746c4
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-03-20 12:40:46 -0700

    completed? TreeNode.py

--- Lab5/TreeNode.py
+++ Lab5/TreeNode.py
@@ -5,13 +5,31 @@ class TreeNode(object):
          self.right = right
 
 def isValidBST(self, root):
+     def helper(n, l=None, u=None):
+          if n == None:
+               return True
+          if l != None and n.val <= l:
+               return False
+          if u != None and n.val >= u:
+               return False
+          if not helper(n.right,n.val,u):
+               return False
+          if not helper(n.left,l,n.val):
+               return False
+          return True
+     return helper(root)
 
 # testing
 n1 = TreeNode(1)
 n2 = TreeNode(2)
 n9 = TreeNode(9)
-n3 = TreeNode(3,n1,None)
-n8 = TreeNode(8,n2,n9)
-n4 = TreeNode(4,n3,n8)
+n3 = TreeNode(3, n1, None)
+n8 = TreeNode(8, n2, n9)
+n4 = TreeNode(4, n3, n8)
 
-print(str(isValidBST(n4,n4)))
+print(isValidBST(n4,n4))  # False
+
+n5 = TreeNode(5)
+n7 = TreeNode(7)
+n6 = TreeNode(6, n5, n7)
+print(isValidBST(n6,n6))  # True
