cd54437ef0da3f3d9fb56e121f062a659d5d6011
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-20 20:24:45 -0700

    Update lab2.py

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,1 +1,16 @@
+class lab2():
+    def isAnagram(self, s:str,t:str) -> bool:
+        if t is None and s is None:
+            return True
+        if(t != None and s == None) or (s != None and t == None) or (len(t)!=len(s)):
+            return False
+        for char in s:
+            if char not in t:
+                return False
+            
+        return True
+    
+# test = lab2()
+# print(test.isAnagram("beauty","ytuaeb"))
+
 
